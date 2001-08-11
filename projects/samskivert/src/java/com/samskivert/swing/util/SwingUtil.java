07f87d855525b62b56d894011bb1923c6b1fdfe1
mdb <mdb@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2001-08-11 22:43:29 +0000

    Added copyright headers.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@231 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/samskivert/src/java/com/samskivert/swing/util/SwingUtil.java
+++ projects/samskivert/src/java/com/samskivert/swing/util/SwingUtil.java
@@ -1,5 +1,22 @@
 //
-// $Id: SwingUtil.java,v 1.2 2001/07/25 23:50:22 mdb Exp $
+// $Id: SwingUtil.java,v 1.3 2001/08/11 22:43:29 mdb Exp $
+//
+// samskivert library - useful routines for java programs
+// Copyright (C) 2001 Michael Bayne
+// 
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package com.samskivert.swing.util;
 

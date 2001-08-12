6a563f89f9bd6362be8a4e8856bdc5edec57ea56
mdb <mdb@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2001-08-12 04:36:58 +0000

    Added license headers, fixed LGPL references.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@250 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/viztool/src/java/com/samskivert/viztool/enum/ZipFileEnumerator.java
+++ projects/viztool/src/java/com/samskivert/viztool/enum/ZipFileEnumerator.java
@@ -1,5 +1,22 @@
 //
-// $Id: ZipFileEnumerator.java,v 1.1 2001/06/14 20:57:15 mdb Exp $
+// $Id: ZipFileEnumerator.java,v 1.2 2001/08/12 04:36:57 mdb Exp $
+// 
+// viztool - a tool for visualizing collections of java classes
+// Copyright (C) 2001 Michael Bayne
+// 
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2.1 of the License, or (at your
+// option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package com.samskivert.viztool.enum;
 

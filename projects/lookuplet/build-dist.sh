eadd4c5a957aed2aa7e6db385c84dea62aec8ec8
mdb <mdb@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2001-08-17 20:52:58 +0000

    *** empty log message ***
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@295 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/lookuplet/build-dist.sh
+++ projects/lookuplet/build-dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: build-dist.sh,v 1.1 2001/08/16 21:03:49 mdb Exp $
+# $Id: build-dist.sh,v 1.2 2001/08/17 20:52:58 mdb Exp $
 #
 # Builds a distribution archive. This should be run from the top-level
 # project directory and it will place the distribution archive into the
@@ -23,7 +23,7 @@ CVS
 macros
 .excludes
 .cvsignore
-*.tgz
+*.tar.gz
 *~
 EOF
 
@@ -35,7 +35,7 @@ mkdir /tmp/$TARGET
 tar --exclude-from=.excludes -cf - * | tar -C /tmp/$TARGET -xf -
 
 # now build the actual archive file
-tar -C /tmp -czf $DISTDIR/$TARGET.tgz $TARGET
+tar -C /tmp -czf $DISTDIR/$TARGET.tar.gz $TARGET
 
 # and clean up after ourselves
 rm -rf /tmp/$TARGET

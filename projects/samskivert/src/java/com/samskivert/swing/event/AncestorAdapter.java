d684ba4711119e5c680b181aafb6ed52d7de499b
shaper <shaper@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2001-12-14 16:00:12 +0000

    Adapter class for the AncestorListener interface.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@511 6335cc39-0255-0410-8fd6-9bcaacd3b74c

---
+++ projects/samskivert/src/java/com/samskivert/swing/event/AncestorAdapter.java
@@ -0,0 +1,34 @@
+//
+// $Id: AncestorAdapter.java,v 1.1 2001/12/14 16:00:12 shaper Exp $
+
+package com.samskivert.swing.event;
+
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+/**
+ * An abstract adapter class for receiving ancestor events. The methods in
+ * this class are empty. This class exists as a convenience for creating
+ * listener objects.
+ *
+ * <p> This class really ought to have been provided as a standard part of
+ * the <code>javax.swing.event</code> package, but somehow the developers
+ * missed it and so we've done their job for them.
+ */
+public abstract class AncestorAdapter implements AncestorListener
+{
+    // documentation inherited
+    public void ancestorAdded (AncestorEvent e) {
+        // nothing to do here
+    }
+
+    // documentation inherited
+    public void ancestorMoved (AncestorEvent e) {
+        // nothing to do here
+    }
+
+    // documentation inherited
+    public void ancestorRemoved (AncestorEvent e) {
+        // nothing to do here
+    }
+}

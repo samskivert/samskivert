9e2c1af4449b2ff8308ffb9b34c62b1f43e96e25
shaper <shaper@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2002-10-01 18:56:32 +0000

    Added copyright header and minor edits to javadoc formatting.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@856 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/samskivert/src/java/com/samskivert/swing/MouseArmingAdapter.java
+++ projects/samskivert/src/java/com/samskivert/swing/MouseArmingAdapter.java
@@ -1,5 +1,22 @@
 //
-// $Id: MouseArmingAdapter.java,v 1.1 2002/10/01 17:31:14 ray Exp $
+// $Id: MouseArmingAdapter.java,v 1.2 2002/10/01 18:56:32 shaper Exp $
+//
+// samskivert library - useful routines for java programs
+// Copyright (C) 2002 Ray Greenwell
+// 
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package com.samskivert.swing;
 
@@ -9,14 +26,15 @@ import java.awt.event.MouseEvent;
 import javax.swing.event.MouseInputAdapter;
 
 /**
- * A mouse arming adapter is a MouseAdapter that translates 'sloppy'
- * clicks into a call to mouseClicked, as well as provides
- * a method that can be used to draw a component as armed or not.
+ * A mouse arming adapter is a {@link MouseInputAdapter} that translates
+ * 'sloppy' clicks into a call to {@link #clicked}, as well as provides a
+ * method that can be used to draw a component as armed or not.
  */
 public abstract class MouseArmingAdapter
 {
     /**
-     * Construct a simple MouseArmingAdapter on the specified component.
+     * Constructs a simple mouse arming adapter for the specified
+     * component.
      */
     public MouseArmingAdapter (Component component)
     {
@@ -24,13 +42,12 @@ public abstract class MouseArmingAdapter
     }
 
     /**
-     * Construct a MouseArmingAdapter with the specified active area
+     * Constructs a mouse arming adapter with the specified active area
      * within the component.
      */
     public MouseArmingAdapter (final Component component, final Shape bounds)
     {
         MouseInputAdapter mia = new MouseInputAdapter() {
-
             // documentation inherited
             public void mousePressed (MouseEvent e)
             {

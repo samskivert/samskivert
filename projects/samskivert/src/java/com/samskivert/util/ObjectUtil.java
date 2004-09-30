962ebeec5372a2ea10081837a639ec60f4153d30
samskivert@gmail.com <samskivert@gmail.com@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2004-09-30 23:49:36 +0000

    Modified implementation of equals() so that it's quicker when the two references point to the same object.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@1510 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/samskivert/src/java/com/samskivert/util/ObjectUtil.java
+++ projects/samskivert/src/java/com/samskivert/util/ObjectUtil.java
@@ -33,7 +33,7 @@ public class ObjectUtil
      */
     public static boolean equals (Object o1, Object o2)
     {
-        return (o1 == null) ? (o2 == null) : o1.equals(o2);
+        return (o1 == o2) || ((o1 != null) && o1.equals(o2));
     }
 
     /**

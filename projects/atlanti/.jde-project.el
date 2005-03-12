01f19518f61b3c01d993389fb464e10c62f215bf
mdb <mdb@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2005-03-12 07:56:57 +0000

    Atlanti is now part of the Game Gardens repository.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@1615 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- projects/atlanti/.jde-project.el
+++
@@ -1,4 +0,0 @@
-(jde-set-variables 
- '(jde-db-source-directories (quote ("/export/local/jdk1.4/src" "/home/mdb/projects/samskivert/src/java" "/home/mdb/work/narya/src/java" "/home/mdb/projects/atlanti/src/java")))
- '(jde-global-classpath (quote ("/home/mdb/projects/samskivert/dist/classes" "/home/mdb/work/narya/dist/classes" "/home/mdb/projects/atlanti/dist/classes")))
- )

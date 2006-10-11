4607cf4b6a4d9a0b1621b09a3c1476cb955d3e53
mdb <mdb@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2006-10-11 18:36:53 +0000

    The Java compiler uses the platform's default encoding when reading source files which explains why Ray's "there are no languages but English" system resulted in complaints and my "named after the African word for community" system did not. Fortunately we can just tell the Java compiler what to expect.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@1942 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- src/java/com/samskivert/jdbc/depot/DepotMarshaller.java
+++ src/java/com/samskivert/jdbc/depot/DepotMarshaller.java
@@ -2,7 +2,7 @@
 // $Id$
 //
 // samskivert library - useful routines for java programs
-// Copyright (C) 2006 Michael Bayne, P&auml;r Winzell
+// Copyright (C) 2006 Michael Bayne, Pär Winzell
 //
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published

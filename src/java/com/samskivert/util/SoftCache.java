7325f92676885ed5fc1a31c4fee5fdc080304088
andrzej <andrzej@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2008-06-17 00:44:14 +0000

    Added an accessor for the underlying map.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@2327 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- src/java/com/samskivert/util/SoftCache.java
+++ src/java/com/samskivert/util/SoftCache.java
@@ -3,7 +3,7 @@
 //
 // samskivert library - useful routines for java programs
 // Copyright (C) 2001-2007 Michael Bayne
-// 
+//
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published
 // by the Free Software Foundation; either version 2.1 of the License, or
@@ -43,6 +43,14 @@ public class SoftCache<K,V>
     public SoftCache ()
     {
         _map = new HashMap<K,SoftReference<V>>();
+    }
+
+    /**
+     * Returns a reference to the underlying map.
+     */
+    public Map<K,SoftReference<V>> getMap ()
+    {
+        return _map;
     }
 
     /**

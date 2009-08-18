2ddd1d0b7adc89268ad03649f1023d6704129f2a
samskivert <samskivert@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2009-08-18 22:01:42 +0000

    Moving ByteEnum into samskivert so that Ray can get optimizey with Narya over the wire enums.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@2613 6335cc39-0255-0410-8fd6-9bcaacd3b74c

---
+++ src/java/com/samskivert/util/ByteEnum.java
@@ -0,0 +1,33 @@
+//
+// $Id$
+//
+// samskivert library - useful routines for java programs
+// Copyright (C) 2001-2008 Michael Bayne
+// 
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package com.samskivert.util;
+
+/**
+ * An interface implemented by enums that can map themselves to a byte. See {@link ByteEnumUtil}
+ * for mapping back from a byte to an enum.
+ */
+public interface ByteEnum
+{
+    /**
+     * Returns the byte value to which to map this enum value.
+     */
+    public byte toByte ();
+}
---
+++ src/java/com/samskivert/util/ByteEnumUtil.java
@@ -0,0 +1,45 @@
+//
+// $Id$
+//
+// samskivert library - useful routines for java programs
+// Copyright (C) 2001-2008 Michael Bayne
+// 
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package com.samskivert.util;
+
+import java.util.EnumSet;
+
+/**
+ * {@link ByteEnum} utility methods.
+ */
+public class ByteEnumUtil
+{
+    /**
+     * Returns the enum value with the specified code in the supplied enum class.
+     *
+     * @exception IllegalArgumentException thrown if the enum lacks a value that maps to the
+     * supplied code.
+     */
+    public static <E extends Enum<E> & ByteEnum> E fromByte (Class<E> eclass, byte code)
+    {
+        for (E value : EnumSet.allOf(eclass)) {
+            if (value.toByte() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException(eclass + " has no value with code " + code);
+    }
+}

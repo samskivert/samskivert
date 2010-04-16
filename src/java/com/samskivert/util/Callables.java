5fa1380c5ce281930549b9348df37445809e7879
ray.j.greenwell <ray.j.greenwell@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2010-04-16 00:56:39 +0000

    @ReplacedBy something in guava.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@2776 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- src/java/com/samskivert/util/Callables.java
+++ src/java/com/samskivert/util/Callables.java
@@ -5,6 +5,8 @@ package com.samskivert.util;
 
 import java.util.concurrent.Callable;
 
+import com.samskivert.annotation.ReplacedBy;
+
 /**
  * Utilities for Callables.
  */
@@ -14,6 +16,7 @@ public class Callables
      * Return a {@link Callable} that merely returns the specified value.
      * No exception will ever be thrown.
      */
+    @ReplacedBy("com.google.common.util.concurrent.Callables#returning()")
     public static <V> Callable<V> asCallable (final V value)
     {
         return new Callable<V>() {

ace6b104e281b3073980f0b92275ee1d27b05d8f
samskivert <samskivert@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2010-09-20 22:31:02 +0000

    Utilities!
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@2890 6335cc39-0255-0410-8fd6-9bcaacd3b74c

---
+++ src/main/java/com/samskivert/net/AddressUtil.java
@@ -0,0 +1,41 @@
+//
+// $Id$
+//
+// samskivert library - useful routines for java programs
+// Copyright (C) 2001-2010 Michael Bayne, et al.
+// 
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package com.samskivert.net;
+
+import java.net.InetSocketAddress;
+
+import com.samskivert.util.StringUtil;
+
+/**
+ * Address related utilities.
+ */
+public class AddressUtil
+{
+    /**
+     * Creates a address to the given host, or the wildcard host if the hostname is
+     * {@link StringUtil#blank}.
+     */
+    public static InetSocketAddress getAddress (String hostname, int port)
+    {
+        return StringUtil.isBlank(hostname) ?
+            new InetSocketAddress(port) : new InetSocketAddress(hostname, port);
+    }
+}

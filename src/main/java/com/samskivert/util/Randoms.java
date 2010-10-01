12906728e5824a2372d5d31b4524be9732892310
karma@deadmoose.com <karma@deadmoose.com@6335cc39-0255-0410-8fd6-9bcaacd3b74c>
2010-10-01 22:44:46 +0000

    That's already a T, so we don't need to cast it.
    git-svn-id: https://samskivert.googlecode.com/svn/trunk@2908 6335cc39-0255-0410-8fd6-9bcaacd3b74c

--- src/main/java/com/samskivert/util/Randoms.java
+++ src/main/java/com/samskivert/util/Randoms.java
@@ -3,7 +3,7 @@
 //
 // samskivert library - useful routines for java programs
 // Copyright (C) 2001-2010 Michael Bayne, et al.
-// 
+//
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published
 // by the Free Software Foundation; either version 2.1 of the License, or
@@ -99,7 +99,7 @@ public class Randoms
     {
         return _r.nextFloat() * high;
     }
-    
+
     /**
      * Returns a pseudorandom, uniformly distributed <code>float</code> value between
      * <code>low</code> (inclusive) and <code>high</code> (exclusive).
@@ -223,9 +223,8 @@ public class Randoms
     /**
      * Shared code for pick and pluck.
      */
-    @SuppressWarnings("unchecked")
     protected <T> T pickPluck (Iterable<? extends T> iterable, T ifEmpty, boolean remove)
-    { 
+    {
         if (iterable instanceof Collection) {
             // optimized path for Collection
             Collection<? extends T> coll = (Collection<? extends T>)iterable;
@@ -237,7 +236,7 @@ public class Randoms
                 // extra-special optimized path for Lists
                 List<? extends T> list = (List<? extends T>)coll;
                 int idx = _r.nextInt(size);
-                return (T)(remove ? list.remove(idx) : list.get(idx));
+                return remove ? list.remove(idx) : list.get(idx);
             }
             // for other Collections, we must iterate
             Iterator<? extends T> it = coll.iterator();

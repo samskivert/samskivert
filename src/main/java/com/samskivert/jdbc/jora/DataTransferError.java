a3399702d7de7696243dad2a62f739c3c32be122
David Hoover <karma@deadmoose.com>
2012-01-03 10:20:55 -0800

    Auld Lang Syne

--- src/main/java/com/samskivert/jdbc/jora/DataTransferError.java
+++ src/main/java/com/samskivert/jdbc/jora/DataTransferError.java
@@ -1,6 +1,6 @@
 //
 // samskivert library - useful routines for java programs
-// Copyright (C) 2001-2011 Michael Bayne, et al.
+// Copyright (C) 2001-2012 Michael Bayne, et al.
 // http://github.com/samskivert/samskivert/blob/master/COPYING
 
 package com.samskivert.jdbc.jora;
